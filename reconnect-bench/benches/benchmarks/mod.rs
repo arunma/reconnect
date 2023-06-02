03fab7b09e817c1dde381d4c9db5f3fa1417179c
Arun Manivannan <arun@arunma.com>
2023-06-03 00:16:33 +0800

    Uploading criterion reports

--- reconnect-bench/benches/benchmarks/mod.rs
+++ reconnect-bench/benches/benchmarks/mod.rs
@@ -2,11 +2,9 @@ use anyhow::anyhow;
 use reconnect_core::config::DiffConfig;
 use reconnect_core::differ::Differ;
 
-pub mod multi_source_postgres_full_table;
 pub mod postgres_full_table;
-pub mod single_source_postgres_full_table;
 
-pub const NUM_ROWS_TO_TEST: [usize; 4] = [10_000, 100_000, 1_000_000, 10_000_000];
+pub const NUM_ROWS_TO_TEST: [usize; 1] = [10_000]; //, 100_000, 1_000_000, 10_000_000];
 pub const DIFF_PCT: f64 = 0.10;
 
 pub fn diff_call(config_string: String) -> anyhow::Result<()> {
